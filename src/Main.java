0af6ebe2b13dba8d79db6f460707e57f8a8d95d6
Sergey Kubetskiy <kubetskiy@gmail.com>
2022-09-01 18:18:01 +0300

    Stream API Homework v.2 Added personsGenerator() Added predicates for filter

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
@@ -7,34 +9,68 @@ public class Main {
     static final int ADULT_HOOD = 27;
     static final int WOMAN_RETIREMENT_AGE = 60;
     static final int MAN_RETIREMENT_AGE = 60;
+    static final int POPULATION_SIZE = 20_000_000;
+
+    static Collection<Person> generatePersons() {
+        Random random = new Random();
+        Collection<Person> persons = new ArrayList<>(POPULATION_SIZE);
+        for (int i = 0; i < POPULATION_SIZE; i++) {
+            persons.add(generatePerson(random));
+        }
+        return persons;
+    }
+
+/*
+    static Collection<Person> generatePersonsWithStream() {
+        Random random = new Random();
+        return IntStream.range(0, POPULATION_SIZE)
+//                .parallel()
+                .mapToObj(i -> generatePerson(random))
+                .collect(Collectors.toList());
+    }
+*/
+
+    static Person generatePerson(Random random) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> surnames = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        return new Person(names.get(random.nextInt(names.size())),
+                surnames.get(random.nextInt(surnames.size())),
+                random.nextInt(100),
+                Sex.values()[random.nextInt(Sex.values().length)],
+                Education.values()[random.nextInt(Education.values().length)]);
+    }
 
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
-        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+//        var a = System.currentTimeMillis();
+        Collection<Person> persons = generatePersons();
+//        var b = System.currentTimeMillis();
+//        Collection<Person> personsFromStream = generatePersonsWithStream();
+//        var c = System.currentTimeMillis();
+//        System.out.println(b - a);
+//        System.out.println(c - b);
 
-        Collection<Person> persons = new ArrayList<>(10_000_000);
-        Random random = new Random();
-        for (int i = 0; i < 10_000_000; i++) {
-            persons.add(new Person(
-                    names.get(random.nextInt(names.size())),
-                    families.get(random.nextInt(families.size())),
-                    random.nextInt(100),
-                    Sex.values()[random.nextInt(Sex.values().length)],
-                    Education.values()[random.nextInt(Education.values().length)])
-            );
-        }
         long numberOfUnderage =  // Счетчик малолеток
                 persons.stream().filter(person -> person.getAge() < EMANCIPATION_AGE).count();
 
-        List<String> cannonFodder = // Таргет военкома
-                persons.stream().filter(person -> person.getAge() >= EMANCIPATION_AGE &&
-                                person.getAge() <= ADULT_HOOD && person.getSex() == Sex.MAN).
-                        map(Person::getSurName).toList();
+        // Таргет военкома
+        List<String> cannonFodder = persons.stream()
+                .filter(filterMilitaryCommissarTarget())
+                .map(Person::getSurName).toList();
 
-        List<Person> laborReserves = persons.stream().  // Трудовые резервы
-                filter(person -> person.getAge() >= EMANCIPATION_AGE && person.getEducation() == Education.HIGHER &&
-                        ((person.getSex() == Sex.MAN && person.getAge() <= MAN_RETIREMENT_AGE) ||
-                                (person.getSex() == Sex.WOMAN && person.getAge() <= WOMAN_RETIREMENT_AGE))).
+        // Трудовые резервы
+        List<Person> laborReserves = persons.stream().
+                filter(filterLaborReserves()).
                 sorted((Comparator.comparing(Person::getSurName))).toList();
+    }
+
+    static Predicate<Person> filterMilitaryCommissarTarget() {
+        return p -> p.getAge() >= EMANCIPATION_AGE &&
+                p.getAge() <= ADULT_HOOD && p.getSex() == Sex.MAN;
+    }
+
+    static Predicate<Person> filterLaborReserves() {
+        return p -> p.getAge() >= EMANCIPATION_AGE && p.getEducation() == Education.HIGHER &&
+                ((p.getSex() == Sex.MAN && p.getAge() <= MAN_RETIREMENT_AGE) ||
+                        (p.getSex() == Sex.WOMAN && p.getAge() <= WOMAN_RETIREMENT_AGE));
     }
 }
